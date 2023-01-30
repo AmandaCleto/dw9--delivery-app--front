9f10d6458b0c43fcddb753d651148d329167249e
AmandaCleto <amanda.cleto.dev@gmail.com>
2023-01-29 22:05:42 -0300

    feat: init project

---
+++ lib/dw9_delivery_app.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class Dw9DeliveryApp extends StatelessWidget {
+  const Dw9DeliveryApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Material();
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:dw9_delivery_app/dw9_delivery_app.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const Dw9DeliveryApp(),
+    );
+  }
+}
