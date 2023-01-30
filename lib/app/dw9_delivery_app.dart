5eee9523d3d19a7c0beca0e839c9b3d8f46d3538
AmandaCleto <amanda.cleto.dev@gmail.com>
2023-01-29 23:05:34 -0300

    feat: create styles and theme

---
+++ lib/app/dw9_delivery_app.dart
@@ -0,0 +1,21 @@
+import 'package:dw9_delivery_app/app/core/ui/styles/app_styles.dart';
+import 'package:dw9_delivery_app/app/core/ui/styles/colors_app.dart';
+import 'package:dw9_delivery_app/app/core/ui/theme/theme_config.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class Dw9DeliveryApp extends StatelessWidget {
+  const Dw9DeliveryApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Delivery App',
+      theme: ThemeConfig.theme,
+      routes: {
+        '/': (context) => Container(),
+      },
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,6 @@
-import 'package:dw9_delivery_app/dw9_delivery_app.dart';
+import 'package:dw9_delivery_app/app/dw9_delivery_app.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: const Dw9DeliveryApp(),
-    );
-  }
+  runApp(const Dw9DeliveryApp());
 }
