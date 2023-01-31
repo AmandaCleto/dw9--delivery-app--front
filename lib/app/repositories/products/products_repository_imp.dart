9c29f81470ac3584b6de27b120036d561ad1aabd
AmandaCleto <amanda.cleto.dev@gmail.com>
2023-01-30 21:30:31 -0300

    feat: create product model and repository

---
+++ lib/app/repositories/products/products_repository_imp.dart
@@ -0,0 +1,31 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'dart:developer';
+
+import 'package:dw9_delivery_app/app/core/exceptions/repository_exceptions.dart';
+import 'package:dw9_delivery_app/app/core/restClient/custom_dio.dart';
+import 'package:dw9_delivery_app/app/models/product_model.dart';
+
+import './products_repository.dart';
+
+class ProductsRepositoryImp implements ProductsRepository {
+  CustomDio dio;
+
+  ProductsRepositoryImp({
+    required this.dio,
+  });
+
+  @override
+  Future<List<ProductModel>> findAllProducts() async {
+    try {
+      final result = await dio.unauth().get('/products');
+
+      return result.data
+          .map<ProductModel>((p) => ProductModel.fromMap(p))
+          .toList();
+    } on Exception catch (e, s) {
+      log('Erro ao buscar produtos', error: e, stackTrace: s);
+      throw RepositoryExceptions(message: 'Erro ao buscar produtos');
+      // TODO
+    }
+  }
+}
