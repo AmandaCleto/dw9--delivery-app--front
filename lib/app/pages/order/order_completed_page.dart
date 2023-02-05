d463bf5f24bc1717f7db1175c2ea4618d10df8a0
AmandaCleto <amanda.cleto.dev@gmail.com>
2023-02-05 18:01:58 -0300

    feat: create completed page

--- lib/app/pages/order/order_completed_page.dart
+++ lib/app/pages/order/order_completed_page.dart
@@ -1,3 +1,6 @@
+import 'package:dw9_delivery_app/app/core/ui/helpers/size_extensions.dart';
+import 'package:dw9_delivery_app/app/core/ui/styles/text_styles.dart';
+import 'package:dw9_delivery_app/app/core/ui/widgets/delivery_button.dart';
 import 'package:flutter/material.dart';
 
 class OrderCompletedPage extends StatefulWidget {
@@ -11,10 +14,36 @@ class _OrderCompletedPageState extends State<OrderCompletedPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('sasa'),
+      body: SingleChildScrollView(
+        child: Center(
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                SizedBox(height: context.percentHeight(0.2)),
+                Image.asset('assets/images/logo_rounded.png'),
+                const SizedBox(height: 10.0),
+                Text(
+                  'Pedido realizado com sucesso, em breve você receberá a confirmação do seu pedido.',
+                  textAlign: TextAlign.center,
+                  style: context.textStyles.textExtraBold.copyWith(
+                    fontSize: 18.0,
+                  ),
+                ),
+                const SizedBox(height: 30.0),
+                DeliveryButton(
+                  width: context.percentWidth(0.8),
+                  label: 'FECHAR',
+                  onPressed: () {
+                    Navigator.pop(context);
+                  },
+                )
+              ],
+            ),
+          ),
+        ),
       ),
-      body: Container(),
     );
   }
 }
